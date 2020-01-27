a7b03507364b62a1407969f842720f008a74d291
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-01-26 23:08:16 -0600

    Docs: Update copyright headers.

--- dob_prompt/prompters/prompter_common.py
+++ dob_prompt/prompters/prompter_common.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# This file is part of 'dob'.
+# This file exists within 'dob-prompt':
 #
-# 'dob' is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   https://github.com/hotoffthehamster/dob-prompt
 #
-# 'dob' is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Copyright © 2019-2020 Landon Bouma. All rights reserved.
 #
-# You should have received a copy of the GNU General Public License
-# along with 'dob'.  If not, see <http://www.gnu.org/licenses/>.
+# This program is free software:  you can redistribute it  and/or  modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3  of the License,  or  (at your option)  any later version  (GPLv3+).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU  General  Public  License  for  more  details.
+#
+# If you lost the GNU General Public License that ships with this software
+# repository (read the 'LICENSE' file), see <http://www.gnu.org/licenses/>.
 
 """"""
 
