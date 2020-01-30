7c01a28f91ab67d55c05c394ee31778310ee45ff
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-01-30 02:51:06 -0600

    License: Update headers.

--- tests/test_integration_tests.py
+++ tests/test_integration_tests.py
@@ -1,17 +1,19 @@
-# This file is part of 'dob_prompt'.
+# This file exists within 'dob-prompt':
 #
-# 'dob_prompt' is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   https://github.com/hotoffthehamster/dob-prompt
 #
-# 'dob_prompt' is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Copyright © 2019-2020 Landon Bouma. All rights reserved.
 #
-# You should have received a copy of the GNU General Public License
-# along with 'dob_prompt'.  If not, see <http://www.gnu.org/licenses/>.
+# This program is free software:  you can redistribute it  and/or  modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3  of the License,  or  (at your option)  any later version  (GPLv3+).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU  General  Public  License  for  more  details.
+#
+# If you lost the GNU General Public License that ships with this software
+# repository (read the 'LICENSE' file), see <http://www.gnu.org/licenses/>.
 
 
 class TestBasicRun(object):
