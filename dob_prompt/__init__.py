1ada661fc5f7c6cd2f2ec0930da5b09f2750ca92
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-02-01 19:24:05 -0600

    Cleanup: Remove unused package module attributes.

--- dob_prompt/__init__.py
+++ dob_prompt/__init__.py
@@ -18,25 +18,13 @@
 """``hamster``, ``hamster``, ``hamster``!!! a cuddly, furry time tracker."""
 
 import os
-import sys
 
 __all__ = (
-    '__arg0name__',
-    '__author__',
-    '__author_email__',
     '__package_name__',
     '__resolve_vers__',
 )
 
-# (lb): These are duplicated in setup.cfg:[metadata], but not sure how to DRY.
-#   Fortunately, they're not likely to change.
-__author__ = 'HotOffThe Hamster'
-__author_email__ = 'hotoffthehamster+dobprompt@gmail.com'
-
-# (lb): Not sure if the package name is available at runtime. Seems kinda meta,
-# anyway, like, Who am I? I just want to avoid hard coding this string in docs.
 __package_name__ = 'dob-prompt'
-__arg0name__ = os.path.basename(sys.argv[0])
 
 
 def __resolve_vers__():
