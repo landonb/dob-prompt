3271ec064929216995fa595eb1e0714df48a819b
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-04-01 13:52:35 -0500

    Bugfix: Pass pkg. name to get_version, lest nark vers. reported.

--- dob_prompt/__init__.py
+++ dob_prompt/__init__.py
@@ -35,5 +35,9 @@ __package_name__ = 'dob-prompt'
 
 
 def get_version(include_head=False):
-    return _get_version(ref_file=__file__, include_head=include_head)
+    return _get_version(
+        package_name=__package_name__,
+        reference_file=__file__,
+        include_head=include_head,
+    )
 
