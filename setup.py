95814427896f3121b55ea7e4ba20f07e93bb8de2
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-04-01 12:20:33 -0500

    Packaging: Bump dep. versions.

--- setup.py
+++ setup.py
@@ -37,10 +37,10 @@ requirements = [
 
     # The heart of Hamster. (Ye olde `hamster-lib`).
     #  https://github.com/hotoffthehamster/nark
-    'nark >= 3.0.1, < 4',
+    'nark >= 3.0.2, < 4',
     # The controller, config, and common output and error tossing code.
     #  https://github.com/hotoffthehamster/dob-bright
-    'dob-bright >= 1.0.3, < 2',
+    'dob-bright >= 1.0.4, < 2',
 ]
 
 # *** Minimal setup() function -- Prefer using config where possible.
