97c3264cf498e0e5e9162995d5817a5454c172f2
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-04-26 00:22:15 -0500

    Packaging: Update dep. version.

--- setup.py
+++ setup.py
@@ -37,10 +37,10 @@ requirements = [
 
     # The heart of Hamster. (Ye olde `hamster-lib`).
     #  https://github.com/hotoffthehamster/nark
-    'nark >= 3.1.0, < 3.2',
+    'nark >= 3.1.1, < 3.2',
     # The controller, config, and common output and error tossing code.
     #  https://github.com/hotoffthehamster/dob-bright
-    'dob-bright >= 1.1.0, < 1.2',
+    'dob-bright >= 1.1.1, < 1.2',
 ]
 
 # *** Minimal setup() function -- Prefer using config where possible.
